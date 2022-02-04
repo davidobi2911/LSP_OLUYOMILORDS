65845ffdb5facb7a7382c7f8c8fc11b92ae31934
USER <USER@10.85.59.145>
2022-02-04 02:05:39 +0100

    first commit

--- large scale programing project/src/org/howard/edu/lsp/assignment1/TextEditior.java
+++ large scale programing project/src/org/howard/edu/lsp/assignment1/TextEditior.java
@@ -4,6 +4,43 @@ public class TextEditior {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		import java.util.Scanner;
+		class Main {
+		  public static void main(String[] args) {
+		    Scanner getInput = new Scanner(System.in);
+		    String name;
+		    String exit = "Goodbye";
+		    System.out.println("String? ");
+		    name = getInput.nextLine();
+		    while(!(name.equals(exit))){
+
+		  
+		    String[] arr = name.split(" ", -2);
+		    System.out.println("Tokens: ");
+		    int total = 0 ;
+		    int product = 1;
+		    for (String el : arr) {
+
+		      System.out.println(el);
+		      int num = Integer.parseInt(el);
+		      total += num;
+		      product *= num;
+		    }
+		  
+		    String output = String.format("Sum: %s", total);
+		    String output2 = String.format("Product: %s", product);
+
+		    System.out.println(output);
+		    System.out.println(output2);
+		    System.out.println("String? ");
+		    name = getInput.nextLine();
+		    
+		    }
+
+		    
+
+		  }
+		}
 
 	}
 
